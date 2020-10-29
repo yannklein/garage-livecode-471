84002037bfcdbcadcd636ec3b3da4f95ab558b9c
yannklein <yann.klein@me.com>
2020-10-29 02:32:52 +0000

    Deploying to gh-pages from  @ b02dbfb61f4cad3c074c66b16fb92b4488aa20ca 🚀

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// TODO: give a badass name to your garage
+const myBadAssGarage = "";
+
+// DON'T CHANGE THIS LINE
+document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
+// //////////////////////
