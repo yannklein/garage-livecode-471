cd0a004461d9df59318f28590918f7583d475aba
Yann Klein <yann.klein@me.com>
2020-10-29 11:19:45 +0900

    get rid of js and update wepback config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
 // TODO: give a badass name to your garage
-import { getCars, postCar } from "./car";
-const myBadAssGarage = "vin-diesel-import";
+const myBadAssGarage = "";
+
 // DON'T CHANGE THIS LINE
 document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
 // //////////////////////
-const submit = document.querySelector("#submit-btn");
-
-getCars();
-
-submit.addEventListener("click", postCar);
