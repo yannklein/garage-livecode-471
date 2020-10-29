f5ad9ae3508d3623cee361c9939db6a996fecfc6
Yann Klein <yann.klein@me.com>
2020-10-29 09:19:54 +0000

    homepage deployed

--- dist/main.js
+++ dist/main.js
@@ -90,15 +90,104 @@
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _car__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./car */ "./src/car.js");
+
 
 // TODO: give a badass name to your garage
-const myBadAssGarage = "";
-
+const myBadAssGarage = "badass-tomo";
 // DON'T CHANGE THIS LINE
 document.querySelector("#garage-name").innerText = myBadAssGarage.replace(/-/g, " ");
 // //////////////////////
+
+// Functions calls
+Object(_car__WEBPACK_IMPORTED_MODULE_0__["fetchCars"])(myBadAssGarage);
+
+// Events handling
+const btn = document.querySelector("#submit-btn");
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  Object(_car__WEBPACK_IMPORTED_MODULE_0__["addCar"])(myBadAssGarage);
+});
+
+
+/***/ }),
+
+/***/ "./src/car.js":
+/*!********************!*\
+  !*** ./src/car.js ***!
+  \********************/
+/*! exports provided: fetchCars, addCar */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fetchCars", function() { return fetchCars; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "addCar", function() { return addCar; });
+// HTML element selection
+const carsList = document.querySelector(".cars-list");
+const brand = document.querySelector("#brand");
+const model = document.querySelector("#model");
+const plate = document.querySelector("#plate");
+const owner = document.querySelector("#owner");
+
+// Function displaying cars on the car-list ul
+const displayCars = (cars) => {
+  cars.forEach((car) => {
+    carsList.insertAdjacentHTML('beforeend',
+      `<div class="car">
+          <div class="car-image">
+            <img src="http://loremflickr.com/280/280/${car.brand} ${car.model}" />
+          </div>
+          <div class="car-info">
+            <h4>${car.brand} ${car.model}</h4>
+            <p><strong>Owner:</strong>${car.owner}</p>
+            <p><strong>Plate:</strong>${car.plate}</p>
+          </div>
+        </div>
+      `);
+  });
+};
+
+// Function fetching all cars from Wagon API
+const fetchCars = (garage) => {
+  const url = `https://wagon-garage-api.herokuapp.com/${garage}/cars`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      const cars = data;
+      carsList.innerHTML = "";
+      displayCars(cars);
+    });
+};
+
+// Function adding car to a garage of Wagon API
+const addCar = (garage) => {
+  const url = `https://wagon-garage-api.herokuapp.com/${garage}/cars`;
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(
+      {
+        brand: brand.value,
+        model: model.value,
+        owner: owner.value,
+        plate: plate.value
+      }
+    )
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      fetchCars();
+    });
+};
+
+
 
 
 /***/ })
